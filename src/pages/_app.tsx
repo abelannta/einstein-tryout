41e1055a6741a9450585a7871747e1232d61604f
Abel Ananta <abelananta9@gmail.com>
2022-11-26 19:43:02 +0700

    feat: add next-progress & header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import NextProgress from "next-progress";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Lorem Ipsum..." />
+        <title>Einstein Tryout</title>
+      </Head>
+      <NextProgress delay={300} options={{ showSpinner: false }} />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="text-4xl flex justify-center items-center h-screen">
-      <h1>Hello</h1>
+    <div className="flex justify-center items-center h-screen flex-col">
+      <p className="text-4xl mb-5">Hello</p>
+      <Link href="/auth/login">
+        <button className="btn btn-primary">Login</button>
+      </Link>
     </div>
   );
 }
