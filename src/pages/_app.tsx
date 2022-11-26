0c5880431740e672054cd6ec399fc2d6dc2ca25f
Abel Ananta <abelananta9@gmail.com>
2022-11-26 19:49:28 +0700

    add: add desc seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,8 +9,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Lorem Ipsum..." />
+        <meta name="theme-color" content="#5800FF" />
+        <meta
+          name="description"
+          content="Aplikasi pintar untuk peminat tryout soal-soal ujian masuk perguruan tinggi populer di Indonesia dengan fitur yang aplikatif dan adaptif."
+        />
         <title>Einstein Tryout</title>
       </Head>
       <NextProgress delay={300} options={{ showSpinner: false }} />
