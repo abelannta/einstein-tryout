916494fd1b8dcc6e72b1ac87014fc600906727b6
Abel Ananta <abelananta9@gmail.com>
2023-02-05 20:48:14 +0700

    fix: bug profile

--- src/modules/profil/index.tsx
+++ src/modules/profil/index.tsx
@@ -34,71 +34,73 @@ export const Profile = () => {
   return (
     <>
       <Basepage>
-        <div className="py-44 container mx-auto">
-          <div className="text-4xl font-bold">Profil</div>
-          <div className="grid grid-cols-4 gap-20 mt-20">
-            <div className="col-span-1">
-              <div className="rounded-full w-full aspect-square mb-10 overflow-hidden border border-gray-300">
-                <Image src={fotoProfil} alt="Foto Profil" />
-              </div>
-              <div className="text-2xl font-bold">Abel Ananta</div>
-              <div className="mt-7">
-                <div className="flex flex-col gap-3">
-                  <div className="flex items-center">
-                    <BiBuildingHouse className="mr-3" />
-                    <p>Universitas Negeri Malang</p>
-                  </div>
-                  <div className="flex items-center">
-                    <BiMap className="mr-3" />
-                    <p>Malang</p>
-                  </div>
-                  <div className="flex items-center">
-                    <BiMailSend className="mr-3" />
-                    <p>abelananta9@gmail.com</p>
+        <div className="bg-white text-black">
+          <div className="py-44 container mx-auto">
+            <div className="text-4xl font-bold">Profil</div>
+            <div className="grid grid-cols-4 gap-20 mt-20">
+              <div className="col-span-1">
+                <div className="rounded-full w-full aspect-square mb-10 overflow-hidden border border-gray-300">
+                  <Image src={fotoProfil} alt="Foto Profil" />
+                </div>
+                <div className="text-2xl font-bold">Abel Ananta</div>
+                <div className="mt-7">
+                  <div className="flex flex-col gap-3">
+                    <div className="flex items-center">
+                      <BiBuildingHouse className="mr-3" />
+                      <p>Universitas Negeri Malang</p>
+                    </div>
+                    <div className="flex items-center">
+                      <BiMap className="mr-3" />
+                      <p>Malang</p>
+                    </div>
+                    <div className="flex items-center">
+                      <BiMailSend className="mr-3" />
+                      <p>abelananta9@gmail.com</p>
+                    </div>
                   </div>
                 </div>
+                <button className="bg-background rounded-xl text-bold w-full p-3 mt-7">
+                  Edit profil
+                </button>
               </div>
-              <button className="bg-background rounded-xl text-bold w-full p-3 mt-7">
-                Edit profil
-              </button>
-            </div>
-            <div className="col-span-3">
-              <div className="tabs mb-10">
-                <Link
-                  href="/profil"
-                  onClick={() => setTabs(TabsStatus.Overview)}
-                  className={`tab tab-lg tab-lifted ${
-                    tabs === TabsStatus.Overview ? "tab-active" : ""
-                  }`}
-                >
-                  Overview
-                </Link>
-                <Link
-                  href={{ query: { tabs: "kegiatan" } }}
-                  className={`tab tab-lg tab-lifted ${
-                    tabs === TabsStatus.Kegiatan ? "tab-active" : ""
-                  }`}
-                >
-                  Kegiatanku
-                </Link>
-                <Link
-                  href={{ query: { tabs: "riwayat" } }}
-                  className={`tab tab-lg tab-lifted ${
-                    tabs === TabsStatus.Riwayat ? "tab-active" : ""
-                  }`}
-                >
-                  Riwayat
-                </Link>
+              <div className="col-span-3">
+                <div className="tabs mb-10">
+                  <Link
+                    href="/profil"
+                    onClick={() => setTabs(TabsStatus.Overview)}
+                    className={`tab tab-lg tab-lifted ${
+                      tabs === TabsStatus.Overview ? "tab-active" : ""
+                    }`}
+                  >
+                    Overview
+                  </Link>
+                  <Link
+                    href={{ query: { tabs: "kegiatan" } }}
+                    className={`tab tab-lg tab-lifted ${
+                      tabs === TabsStatus.Kegiatan ? "tab-active" : ""
+                    }`}
+                  >
+                    Kegiatanku
+                  </Link>
+                  <Link
+                    href={{ query: { tabs: "riwayat" } }}
+                    className={`tab tab-lg tab-lifted ${
+                      tabs === TabsStatus.Riwayat ? "tab-active" : ""
+                    }`}
+                  >
+                    Riwayat
+                  </Link>
+                </div>
+                {tabs === TabsStatus.Overview ? (
+                  <OverviewProfile />
+                ) : tabs === TabsStatus.Kegiatan ? (
+                  <Kegiatanku />
+                ) : tabs === TabsStatus.Riwayat ? (
+                  <Riwayat />
+                ) : (
+                  <OverviewProfile />
+                )}
               </div>
-              {tabs === TabsStatus.Overview ? (
-                <OverviewProfile />
-              ) : tabs === TabsStatus.Kegiatan ? (
-                <Kegiatanku />
-              ) : tabs === TabsStatus.Riwayat ? (
-                <Riwayat />
-              ) : (
-                <OverviewProfile />
-              )}
             </div>
           </div>
         </div>
