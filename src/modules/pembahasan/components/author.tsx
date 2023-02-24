e61f769d5046997255132ca5f85592666541a18d
Abel Ananta <abelananta9@gmail.com>
2023-02-24 23:34:20 +0700

    fix: author pembahasan center align

--- src/modules/pembahasan/components/author.tsx
+++ src/modules/pembahasan/components/author.tsx
@@ -4,11 +4,9 @@ export const AuthorPembahasan = () => {
       <div className="text-md md:text-xl font-bold pb-4">Mentor Pembahas</div>
       <div className="flex gap-5">
         <div className="rounded-full bg-gray-400 w-20 aspect-square"></div>
-        <div>
-          <div className="">
-            <h3 className="text-base md:text-lg">Joko Sulistiawan</h3>
-            <p className="text-base">Mentor Biologi</p>
-          </div>
+        <div className="flex flex-col justify-center">
+          <h3 className="text-base md:text-lg">Joko Sulistiawan</h3>
+          <p className="text-base">Mentor Biologi</p>
         </div>
       </div>
     </div>
